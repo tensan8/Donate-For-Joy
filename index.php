1d41a1eb2a0ea8ca1d8fed9eb51d0aae30288978
Anjay56 <101229576@students.swinburne.edu.my>
2021-09-01 18:20:47 +0700

    Homepage still in progress

--- index.php
+++ index.php
@@ -13,6 +13,32 @@
     <div class="first-section">
         <?php include("navbar.php") ?>
     </div>
+    <div class="second-section">
+        <h2>Panti Asuhan Bhakti Luhur Alma <br/>Needs Your Help !</h2>
+        <div class="first-image-section">
+            <img src="images/panti1.jpg" alt="First Image"/>
+            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
+                Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum</p>
+        </div>
+        <div class="second-image-section">
+
+        </div>
+    </div>
+
+    <div class="third-section">
+        
+    </div>
+
+    <div class="fourth-section">
+        
+    </div>
+
+    <!-- Sini tensan (Progress bar) -->
+    <div class="fifth-section">
+        
+    </div>
+  
+    
 
     <script>
         $(function() {
