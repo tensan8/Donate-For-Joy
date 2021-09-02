35c2568b8848c9e7a7f3a0e01cf2e5559813f816
Anjay56 <101229576@students.swinburne.edu.my>
2021-09-03 00:15:28 +0700

    Updated first, second, and third section of the home page

--- index.php
+++ index.php
@@ -10,44 +10,84 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
 <body>
+    <!-- FIRST SECTION OF THE HOME PAGE  -->
     <div class="first-section">
         <?php include("navbar.php") ?>
+        <div class="first-section-image-content">
+            <h1>Help The Children By Donating</h1>
+            <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+        </div>
     </div>
+
+    <!-- SECOND SECTION OF THE HOME PAGE (CAROUSEL) -->
     <div class="second-section">
         <h2>Panti Asuhan Bhakti Luhur Alma <br/>Needs Your Help !</h2>
-        <div class="first-image-section">
-            <img src="images/panti1.jpg" alt="First Image"/>
-            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
-                Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum</p>
-        </div>
-        <div class="second-image-section">
+        <div class="first-carousel-container">
+            <div class="first-carousel">
+                <div class="first-slide">
+                    <img src="images/panti1.jpg" alt="Picture of the kids" />
+                </div>
+                <div class="first-slide">
+                    <img src="images/panti2.jpg" alt="Picture of the orphanage's ceiling"/>
+                </div>
+                <div class="first-slide">
+                    <img src="images/panti3.jpg" alt="Picture of the orphanage's ceiling"/>
+                </div>
 
+                <div class="first-dots-container">
+                    <span class="first-dot" onclick="currentSlides(1)"></span>
+                    <span class="first-dot" onclick="currentSlides(2)"></span>
+                    <span class="first-dot" onclick="currentSlides(3)"></span>
+                </div>
+            </div>
+
+            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum</p>
+        </div>
+    
+        <div class="second-carousel-container">
+            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum</p>
+
+            <div class="second-carousel">
+                <div class="second-slide">
+                    <img src="images/panti4.JPG" alt="Picture of the kids" />
+                </div>
+                <div class="second-slide">
+                    <img src="images/panti5.jpg" alt="Picture of the orphanage's ceiling"/>
+                </div>
+                <div class="second-slide">
+                    <img src="images/panti6.jpg" alt="Picture of the orphanage's ceiling"/>
+                </div>
+
+                <div class="second-dots-container">
+                    <span class="second-dot" onclick="currentSlides(1)"></span>
+                    <span class="second-dot" onclick="currentSlides(2)"></span>
+                    <span class="second-dot" onclick="currentSlides(3)"></span>
+                </div>
+            </div>
         </div>
     </div>
 
+
+    <!-- THIRD SECTION OF THE HOME PAGE -->
+
     <div class="third-section">
-        
+    <iframe width="1280" height="720" src="https://www.youtube.com/embed/sKtCP6htHqw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. 
+            It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
+            Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+            ooked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
+            and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, 
+            written in 45 BC. </p>
     </div>
 
     <div class="fourth-section">
         
     </div>
 
-    <!-- Sini tensan (Progress bar) -->
     <div class="fifth-section">
         
     </div>
-  
-    
-
-    <script>
-        $(function() {
-            $(".nav-bar div:nth-child(2) a").removeClass("secondary-color-bg");
-            $(".nav-bar div:last-child a").removeClass("secondary-color-bg");
-            $(".nav-bar div:first-child a").removeClass("secondary-color-bg");
-            $(".nav-bar div:first-child a").addClass("secondary-color-bg");
-        });
-    </script>
-
+    <script src="script/index.js"></script>
 </body>
 </html>
---
+++ script/index.js
@@ -0,0 +1,63 @@
+
+$(function() {
+    $(".nav-bar div:nth-child(2) a").removeClass("secondary-color-bg");
+    $(".nav-bar div:last-child a").removeClass("secondary-color-bg");
+    $(".nav-bar div:first-child a").removeClass("secondary-color-bg");
+    $(".nav-bar div:first-child a").addClass("secondary-color-bg");
+
+
+    let firstSlideIndex = 1;
+    let secondSlideIndex = 1;
+
+
+    showSlides(firstSlideIndex, 1);
+    showSlides(secondSlideIndex, 2);
+
+    $(".first-carousel").on("click", function() {
+        showSlides(firstSlideIndex += 1, 1);
+    });
+
+    $(".second-carousel").on("click", function() {
+        showSlides(secondSlideIndex += 1, 2);
+    })
+
+    function currentSlide(n, order) {
+        if (order == 1) {
+            showSlides(firstSlideIndex = n, order);
+        }
+        else if (order == 2) {
+            showSlides(secondSlideIndex = n, order);
+        }
+    }
+
+
+
+    function showSlides(n, order) {
+        if (order == 1) {
+            if (n > $(".first-slide").length) {firstSlideIndex = 1;}
+            if (n < 1) {firstSlideIndex = $(".first-slide").length}
+
+    
+            $(".first-slide").css("display", "none");
+            
+            $(".first-dot").removeClass("active");
+            
+            $($(".first-slide")[firstSlideIndex - 1]).css("display", "block");
+            $($(".first-dot")[firstSlideIndex - 1]).addClass("active");
+        }
+        else if (order == 2) {
+            if (n > $(".second-slide").length) {secondSlideIndex = 1;}
+            if (n < 1) {secondSlideIndex = $(".second-slide").length}
+
+    
+            $(".second-slide").css("display", "none");
+            
+            $(".second-dot").removeClass("active");
+            
+            $($(".second-slide")[secondSlideIndex - 1]).css("display", "block");
+            $($(".second-dot")[secondSlideIndex - 1]).addClass("active");
+        }
+    }
+
+});
+    
